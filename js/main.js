e400f2931ded83de9ef69d0a63c12ef86c4b464c
dilshandcs <dilshan.c.subasinghe@gmail.com>
2018-09-23 12:40:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -77,3 +77,24 @@ function solarTotal(){
 		uticos.value = ut_price;
 		totsavin.value = total_saving;
 }
+
+	   (function(){
+	      emailjs.init("user_l660vN5g7aBh7b3cDvlOy");
+	   })();
+	   
+	   function sendEmails(){
+		   var emailId =	document.getElementById("emailId").value;
+		   var sysiz =	document.getElementById("syssize").value;
+		   var	noofsolarpanel = document.getElementById("noofsolarpanels").value;
+		   var	are = document.getElementById("area").value;
+		   var totsyscos = document.getElementById("totsyscost").value;
+		   var	uticos = document.getElementById("uticost").value;
+		   var	totsavin = document.getElementById("totsaving").value;
+		   
+		   emailjs.send("gmail", "template_LrBpUKiF", {"emailId":emailId,"sysiz":sysiz,"noofsolarpanel":noofsolarpanel,"are":are,"totsyscos":totsyscos,"uticos":uticos,"totsavin":totsavin})	 	       
+		    .then(function(response) {
+		        console.log('SUCCESS!', response.status, response.text);
+		     }, function(error) {
+		        console.log('FAILED...', error);
+		     });	   
+	   }
